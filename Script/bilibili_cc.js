7a2ff486015f7e3d581e71818f26ba4b5d2eebed
Chang9an <114060477+Chang9an@users.noreply.github.com>
2022-11-15 14:30:44 +0800

    Update bilibili_cc.js

--- Script/bilibili_cc.js
+++ Script/bilibili_cc.js
@@ -3,12 +3,9 @@
 > 应用名称：哔哩哔哩
 > 软件版本：7.1.0
 > 下载地址：https://apps.apple.com/cn/app/id736536022
-> 脚本作者：Cuttlefish
-> 微信账号：墨鱼手记
+
 > 更新时间：2022-11-01
-> 通知频道：https://t.me/ddgksf2021
-> 投稿助手：https://t.me/ddgksf2013_bot
-> 问题反馈：📮 ddgksf2013@163.com 📮
+>
 > 特别说明：本脚本仅供学习交流使用，禁止转载售卖
  
 [rewrite_local]
