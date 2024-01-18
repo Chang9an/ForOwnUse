83ba60027a52dbaf22da492b145f5b46b5076813
Chang9an <114060477+Chang9an@users.noreply.github.com>
2024-01-18 23:29:48 +0800

    更新 fix-vvebo-user-timeline.js

--- Script/fix-vvebo-user-timeline.js
+++ Script/fix-vvebo-user-timeline.js
@@ -27,11 +27,11 @@ if (url.includes("remind/unread_count")) {
 } else if (url.includes("profile/statuses/tab")) {
   let data = JSON.parse($response.body);
   let statuses = data.cards
-    。map((card) => (card.card_group ? card.card_group : card))
-    。flat()
-    。filter((card) => card.card_type === 9)
-    。map((card) => card.mblog)
-    。map((status) => (status.isTop ? { ...status, label: "置顶" } : status));
+    .map((card) => (card.card_group ? card.card_group : card))
+    .flat()
+    .filter((card) => card.card_type === 9)
+    .map((card) => card.mblog)
+    .map((status) => (status.isTop ? { ...status, label: "置顶" } : status));
   let sinceId = data.cardlistInfo.since_id;
   $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
 } else {
