404db09b4914aee9786cb39fbaa0bd5bb8b32615
Chang9an <114060477+Chang9an@users.noreply.github.com>
2024-01-18 23:22:21 +0800

    Update fix-vvebo-user-timeline.js

--- Script/fix-vvebo-user-timeline.js
+++ Script/fix-vvebo-user-timeline.js
@@ -1,3 +1,18 @@
+/******************************
+QuantumultX 修复 vvebo 用户主页的显示脚本
+参考：https://raw.githubusercontent.com/suiyuran/stash/main/override/fix-vvebo.stoverride
+https://raw.githubusercontent.com/bin64/Scripts/main/QuantumultX/vvebo.js
+*************************
+
+[mitm]
+hostname = api.weibo.cn
+[rewrite_local]
+^https:\/\/api\.weibo\.cn\/2\/users\/show\? url script-request-header https://raw.githubusercontent.com/Chang9an/ForOwnUse/main/Script/fix-vvebo-user-timeline.js
+^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-request-header https://raw.githubusercontent.com/Chang9an/ForOwnUse/main/Script/fix-vvebo-user-timeline.js
+^https:\/\/api\.weibo\.cn\/2\/statuses\/user_timeline\? url script-response-body https://raw.githubusercontent.com/Chang9an/ForOwnUse/main/Script/fix-vvebo-user-timeline.js
+*************************
+
+*****************************************/
 let url = $request.url;
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);
